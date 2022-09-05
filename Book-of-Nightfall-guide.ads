02dbd7bd99305ec0c7dedc0d2d6ddfb0846bfcf8
chaimalk <108950943+chaimalk@users.noreply.github.com>
2022-09-05 21:35:19 +0200

    Create Book-of-Nightfall-guide.ads

---
+++ Book-of-Nightfall-guide.ads
@@ -0,0 +1,55 @@
+{
+	"Playstore": {
+
+		"AdsController": {
+
+			"NetworkAds": "admob",
+
+			"BannerAdmob": "ca-app-pub-7448804041969308/2244512352",
+			"InterstitialAdmob": "ca-app-pub-7448804041969308/8618349011",
+
+			"BannerFacebook": "3258063057814346_3258063487814303",
+			"InterstitialFacebook": "3258063057814346_3258063847814267",
+			
+			"unityGameID": "4916073",
+			"UnityPlacement": "Interstitial_Android",
+
+			"StartappAds": "208610175",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": true,
+			"Rating": true
+
+		}
+
+	},
+	"Huaweistore": {
+
+		"AdsController": {
+
+			"NetworkAds": "startapp",
+
+			"BannerAdmob": "ca-app-pub-8082149907872048/3898325980",
+			"InterstitialAdmob": "ca-app-pub-8082149907872048/8781442418",
+
+			"BannerFacebook": "1347348165628536_1347349145628438",
+			"InterstitialFacebook": "1347348165628536_1347353132294706",
+
+			"StartappAds": "211203742",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": false,
+			"Rating": false
+
+		}
+
+	}
+}
