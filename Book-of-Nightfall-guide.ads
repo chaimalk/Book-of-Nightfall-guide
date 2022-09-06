f95287d4bcd0aa86ac48836bbff31d55aaafad82
chaimalk <108950943+chaimalk@users.noreply.github.com>
2022-09-06 22:33:17 +0200

    Update Book-of-Nightfall-guide.ads

--- Book-of-Nightfall-guide.ads
+++ Book-of-Nightfall-guide.ads
@@ -3,7 +3,7 @@
 
 		"AdsController": {
 
-			"NetworkAds": "admob",
+			"NetworkAds": "unity",
 
 			"BannerAdmob": "ca-app-pub-7448804041969308/2244512352",
 			"InterstitialAdmob": "ca-app-pub-7448804041969308/8618349011",
